e1e6adde488fd10e82f01e9aad467cd92984cb43
Edward Liu <edwardliu@berkeley.edu>
2017-02-25 00:09:28 -0800

    enabling functionality

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ app.get('/webhook/', function(req, res) {
 function sendText(sender, text) {
 	let messageData = {text: text}
 	request({
-		url:"https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token : token},
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token: token},
 		method: "POST",
 		json: {
 			recipient: {id: sender},
-			message : messageData
+			message: messageData
 		}
-	}, function (error, response, body) {
+	}, function(error, response, body){
 		if (error) {
 			console.log("sending error")
 		} else if (response.body.error) {
@@ -50,17 +50,16 @@ function sendText(sender, text) {
 
 app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
+	for (let i = 0; i < messaging_events.length; i ++){
 		let event = messaging_events[i]
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			sendText(sender, "Text echo: " + text.substring(0, 100))
+			sendText(sender, "Text echo: " + text.substring(0,100))
 		}
 	}
 	res.sendStatus(200)
 })
-
 
 app.listen(app.get('port'), function() {
 	console.log("running: port")
